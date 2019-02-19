030da298718bc326176891f57c648805ff913e11
Nitin Goyal <nitingoyal.dev@gmail.com>
2019-02-19 20:51:09 +0530

    import improved

--- lists/recipients/getTotalRecipients/action.js
+++ lists/recipients/getTotalRecipients/action.js
@@ -1,5 +1,5 @@
 import { logger } from '../../lib/index';
-import Recipients from '../../lib/recipients/index';
+import Recipients from '../../lib/recipients';
 
 export default function respond(event, cb) {
   logger().info('= getTotalRecipients.action', JSON.stringify(event));
