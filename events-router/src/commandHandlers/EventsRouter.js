89843d096ba32a6c3a54cb6d007ecd316421b628
Nitin Goyal <nitingoyal.dev@gmail.com>
2019-02-28 01:23:46 +0530

    import fix

--- events-router/src/commandHandlers/EventsRouter.js
+++ events-router/src/commandHandlers/EventsRouter.js
@@ -1,4 +1,4 @@
-import R from 'ramda';
+import * as R from 'ramda';
 import Promise from 'bluebird';
 import SubscriptionRepo from '../repositories/Subscription';
 import KinesisNotifier from '../notifiers/KinesisNotifier';
